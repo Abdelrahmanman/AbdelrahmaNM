730556ac8793856ba7a5cf66ea94bacf8eda8c45
Abdelrahman <AbdElRahman.man@hotmail.com>
2018-02-15 20:50:09 +0200

    website
    files uploaded

---
+++ js/tricks.js
@@ -0,0 +1,62 @@
+$('document').ready(function(){
+  $('nav .navbar .navIcon').on('click',function(){
+    $('.navPopup').toggleClass('topNavAnimation');
+  })
+
+
+
+  $('.me,.home').click(function () {
+    $('html, body').animate({
+     scrollTop : $('#' + $(this).data('value')).offset().top - 150}, 700);
+   });
+
+
+/*
+
+  $( window ).scroll(function() {
+    if($(this).scrollTop() > window.innerHeight){
+      $('.navPopup').addClass('topNavAnimation');
+    }
+    else {
+      $('.navPopup').removeClass('topNavAnimation');
+    }
+   });
+
+*/
+   $(window).scroll(function(){
+       if($(this).scrollTop()>= 500)
+         {$(".backToTop").slideDown();}
+       else
+         {$(".backToTop").slideUp();}
+     })
+   $(".backToTop").click(function(){
+     $("html,body").animate({scrollTop:0},500);
+   })
+
+
+
+   $("body").niceScroll({
+   cursorwidth:6,
+   cursorcolor:'#0093f9',
+   cursorborder:'none',
+   cursorborderradius:4,
+   autohidemode:'leave'
+   });
+
+
+
+
+   $('.contactMe').click(function(){
+    $('.contactMePopup').slideDown();
+    $('.overlaycontact').fadeIn();
+    $('nav .navbar .navIcon').slideUp();
+   })
+
+
+   $('.contactMePopup i').click(function(){
+      $('.contactMePopup').slideUp();
+      $('.overlaycontact').fadeOut();
+      $('nav .navbar .navIcon').slideDown();
+   })
+
+})
